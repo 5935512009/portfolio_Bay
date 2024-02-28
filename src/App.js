f024a6f67c4cdfdcc088d01be4ee3c68f9a5094e
5935512009 <Pasut2541@hotmail.com>
2024-02-28 08:39:21 +0700

    Update:{App.js}

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Main/>
+        <Main/><br/>
+        <Content></Content><br/>
+        <Footer/><br/>
       </header>
     </div>
   );
