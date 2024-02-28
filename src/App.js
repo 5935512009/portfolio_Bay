c27a3ea9ab067f5aef98db0dc0da4bf5023d5aae
5935512009 <Pasut2541@hotmail.com>
2024-02-28 12:40:07 +0700

    Update;{App,css,App.js}

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Content from "./components/content/Content";
 import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
+import './App.css'
 import Narbar from "./components/navBar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Narbar/><br/>
-        <Main/><br/>
+        <Narbar/>
+        <Main/>
         <Content></Content><br/>
         <Footer/><br/>
       </header>
